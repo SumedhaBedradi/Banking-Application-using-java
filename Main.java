2c9c371379d7c9adecb802cfd4c4e52a445308b2
SumedhaBedradi <144426153+SumedhaBedradi@users.noreply.github.com>
2024-03-20 22:57:05 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+          Account sumedhasAccount = new Account("121212",0,"Sumedha","sumedha@abc.com","12345");
+
+		        sumedhasAccount.DepositMoney(500);
+		        sumedhasAccount.DepositMoney(150);
+		        sumedhasAccount.WithDrawMoney(100);
+		    
+		    }
+	}
+
+
